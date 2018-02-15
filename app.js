6f2a877386a04a645f09bd710f593b8ea79ad128
Gerard <gkhynes@gmail.com>
2018-02-15 21:26:04 +0000

    Initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+var width = 500;
+var height = 500;
+var padding = 20;
+
+// prettier-ignore
+var yScale = d3.scaleLinear()
+                .domain(d3.extent(birthData2011, d => d.lifeExpectancy))
+                .range([height - padding, padding]); // flips the y-axis
+
+// prettier-ignore
+var xScale = d3.scaleLinear()
+                .domain(d3.extent(birthData2011, d => d.births / d.population))
+                .range([padding, width - padding]);
+
+// prettier-ignore
+var colorScale = d3.scaleLinear()
+                    .domain(d3.extent(birthData2011, d => d.population / d.area))
+                    .range(["lightgreen", "black"]);
+
+// prettier-ignore
+var radiusScale = d3.scaleLinear()
+                    .domain(d3.extent(birthData2011, d => d.births))
+                    .range([2, 40]);
+
+// prettier-ignore
+d3.select("svg")
+    .attr("width", width)
+    .attr("height", height)
+  .selectAll("circle")
+  .data(birthData2011)
+  .enter()
+  .append("circle")
+    .attr("cx", d => xScale(d.births / d.population))
+    .attr("cy", d => yScale(d.lifeExpectancy))
+    .attr("fill", d => colorScale(d.population / d.area))
+    .attr("r", d => radiusScale(d.births));
